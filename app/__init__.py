4a9a99291770a105200ba9e9fb59eff033e055e2
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2024-07-03 22:21:43 +0530

    update home page name

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask import render_template
 from .models import db
+from .routes import auth
 
 
 def create_app():
@@ -19,15 +20,17 @@ def create_app():
 
     print("database initialized")
 
-    # registering auth.Auth blueprint to the app
+    # registering auth.auth_ blueprint to the app
+    app.register_blueprint(auth.auth_)
+
 
     # a simple page to check server status
     @app.route('/check')
     def message():
-        return "<h1>Site is Live...</h1><p>this is demo paragraph</p>"
+        return "<h1>Site is Live...</h1>"
 
     @app.route('/')
     def home():
-        return render_template("pages/login.html")
+        return render_template("pages/home.html")
 
     return app
