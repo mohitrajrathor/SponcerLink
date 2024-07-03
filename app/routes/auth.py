c4215df98db5261dbb38b81770109e69607e2133
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2024-07-03 22:26:05 +0530

    added login and register routes

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,3 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, session
+from ..models import *
 
-auth = Blueprint(__name__, "auth")
+auth_ = Blueprint("auth", __name__, url_prefix='/auth')
+
+# registration 
+@auth_.route('/register')
+def register():
+    return render_template("pages/register.html")
+
+# login page
+@auth_.route('/login')
+def login():
+    return render_template('pages/login.html')
